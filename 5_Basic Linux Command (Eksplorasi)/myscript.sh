8335e55c1300a6469d6881f94dbd45272a097306
Rhesna Aridlo Setiawan <rhesna1922@gmail.com>
2023-03-08 17:14:01 +0700

    doc: add shell script

---
+++ 5_Basic Linux Command (Eksplorasi)/myscript.sh
@@ -0,0 +1,19 @@
+#mengahapus screen
+clear
+
+#membuat variabel nama, tanggal lahir, dan kota
+nama='Rhesna Aridlo Setiawan'
+tanggallahir='21 Februari 2002'
+kota='Pati'
+
+#menggunakan fungsi echo untuk memanggil variabel
+echo "Nama saya adalah $nama" 
+echo "Lahir pada $tanggallahir" 
+echo "Tempat di kabupaten $kota"
+
+
+#menampilkan fungsi date
+echo "Pada ";date
+echo -n "tekan enter untuk melanjutkan..."
+read
+clear
